b80e2e28f6b9383c6af2b820a99544c6fa40f9a9
Eugene Maysyuk <eugene.maysyuk@zendesk.com>
2024-05-20 09:58:34 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,19 @@
+val versionHttp4S = "1.0.0-M38"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "scala-woof-logger-issue",
+    version := "0.1",
+    organization := "com.may",
+    scalaVersion := "3.4.1",
+    libraryDependencies ++= Seq(
+      "org.typelevel" %% "cats-core" % "2.10.0",
+      "org.typelevel" %% "cats-effect" % "3.5.1",
+      "org.legogroup" %% "woof-core" % "0.7.0",
+      "org.http4s" %% s"http4s-ember-client" % versionHttp4S,
+      "org.http4s" %% s"http4s-ember-server" % versionHttp4S,
+      "org.http4s" %% s"http4s-dsl" % versionHttp4S,
+      "org.http4s" %% s"http4s-circe" % versionHttp4S,
+      "co.fs2" %% "fs2-core" % "3.5.0",
+    )
+  )
---
+++ src/main/scala/com/may/main.scala
@@ -0,0 +1,63 @@
+package com.may
+
+import cats.effect.{IO, IOApp}
+import org.legogroup.woof.{*, given}
+import Logger.*
+import cats.effect.kernel.Resource
+import fs2.Stream
+import org.http4s.server.Server
+import com.comcast.ip4s.*
+import org.http4s.HttpRoutes
+import org.http4s.ember.server.EmberServerBuilder
+import cats.syntax.all.*
+import org.http4s.dsl.io.*
+
+object main extends IOApp.Simple:
+  case class Agent(accountId: Long, agent: Long)
+
+  given Filter = Filter.everything
+  given Printer = ColorPrinter()
+
+  def run: IO[Unit] =
+    for
+      given Logger[IO] <- DefaultLogger.makeIo(Output.fromConsole)
+      _                <- runWithLogger.withLogContext("application", "app").withLogContext("environment", "dev")
+    yield ()
+
+  private def runWithLogger(using logger: Logger[IO]): IO[Unit] = for {
+    _ <- logger.info(s"Starting app")
+    _ <- Stream
+      .resource(resources)
+      .flatMap { httpServer =>
+        for {
+          given Logger[IO] <- DefaultLogger.makeIo(Output.fromConsole)
+          _ <- Logger[IO].warn("Before exception") // TODO: doesn't print anything
+        } yield ()
+        Stream.eval(httpServer.useForever).concurrently {
+          throw new RuntimeException("Catch me and log to console")
+        }
+      }
+      .handleError { e =>
+        Stream.eval {
+          logger.error(s"An error occured: $e") // TODO: doesn't print anything
+        }
+      }
+      .compile
+      .drain
+  } yield ()
+
+  private def resources(using logger: Logger[IO]): Resource[IO, (Resource[IO, Server])] =
+    for
+      _ <- Resource.eval(Logger[IO].info(s"Initializing resources"))
+      httpServer = EmberServerBuilder
+        .default[IO]
+        .withHost(host"0.0.0.0")
+        .withPort(port"9006")
+        .withHttpApp(
+          HttpRoutes.of[IO] {
+            case GET -> Root / "health" => Ok("works")
+            case _ => Ok("works")
+          }.orNotFound
+        )
+        .build
+    yield httpServer
